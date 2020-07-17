c8666ab03e043b6c7c9834777e5424a9b8be89ea
Sahan Wickramage <35292360+SahanWickramage@users.noreply.github.com>
2020-07-17 12:25:17 +0530

    added scheduler
    added scheduler source
    added scheduler tb

--- HDL-mean-filter/HDL-mean-filter.runs/synth_1/runme.sh
+++ HDL-mean-filter/HDL-mean-filter.runs/synth_1/runme.sh
@@ -11,9 +11,9 @@ echo "Please update the PATH and LD_LIBRARY_PATH variables below, before executi
 exit
 
 if [ -z "$PATH" ]; then
-  PATH=F:/xilinx/Vivado/2019.2/ids_lite/ISE/bin/nt64;F:/xilinx/Vivado/2019.2/ids_lite/ISE/lib/nt64:F:/xilinx/Vivado/2019.2/bin
+  PATH=D:/Windows/ProgramFiles/Xilinx/Vivado/2019.2/ids_lite/ISE/bin/nt64;D:/Windows/ProgramFiles/Xilinx/Vivado/2019.2/ids_lite/ISE/lib/nt64:D:/Windows/ProgramFiles/Xilinx/Vivado/2019.2/bin
 else
-  PATH=F:/xilinx/Vivado/2019.2/ids_lite/ISE/bin/nt64;F:/xilinx/Vivado/2019.2/ids_lite/ISE/lib/nt64:F:/xilinx/Vivado/2019.2/bin:$PATH
+  PATH=D:/Windows/ProgramFiles/Xilinx/Vivado/2019.2/ids_lite/ISE/bin/nt64;D:/Windows/ProgramFiles/Xilinx/Vivado/2019.2/ids_lite/ISE/lib/nt64:D:/Windows/ProgramFiles/Xilinx/Vivado/2019.2/bin:$PATH
 fi
 export PATH
 
@@ -24,7 +24,7 @@ else
 fi
 export LD_LIBRARY_PATH
 
-HD_PWD='E:/Acadamics/Semester 7/HDL/HDL-project/HDL-mean-filter/HDL-mean-filter.runs/synth_1'
+HD_PWD='D:/MyStuff/Projects/Vivado/HDL-project/HDL-mean-filter/HDL-mean-filter.runs/synth_1'
 cd "$HD_PWD"
 
 HD_LOG=runme.log
